f9db7fa57f8eec6dd4b44b7f4f7b47d76d4b0fcc
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-03-14 15:33:39 +0100

    pandora.hpp: updated documentation.

--- include/pandora.hpp
+++ include/pandora.hpp
@@ -1,4 +1,19 @@
+// Copyright (c) 2013, German Neuroinformatics Node (G-Node),
+//                     Adrian Stoewer (adrian.stoewer@rz.ifi.lmu.de),
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
 
+/**
+ * @file pandora.hpp
+ * @brief Main header for the public API.
+ *
+ * @namespace pandora
+ * @brief This namespace contains all declarations of the public API.
+ */
 
 #include <pandora/Group.hpp>
 #include <pandora/File.hpp>
