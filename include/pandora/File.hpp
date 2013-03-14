2ab9f7dbc4649c76bf3a5c84a46df9bb4b2c7404
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-03-14 17:11:04 +0100

    File: update documentation.

--- include/pandora/File.hpp
+++ include/pandora/File.hpp
@@ -1,60 +1,126 @@
+// Copyright (c) 2013, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+/**
+ * @file pandora/File.hpp
+ * @brief Definition of the class file.
+ */
+
 #ifndef PAN_FILE_H_INCLUDED
 #define PAN_FILE_H_INCLUDED
 
 #include <iterator>
 #include <string>
-
 #include <H5Cpp.h>
 
 #include <pandora/Group.hpp>
 
 namespace pandora {
-
 class Block;
 
+/**
+ * Class that represents a pandora file.
+ */
 class File
 {
 
 private:
 
+  /* prefix for IDs: currently not used */
   std::string prefix;
+  /* the opened HDF5 file */
   H5::H5File  h5file;
+  /* groups representing different sections of the file */
   Group       root, metadata, data;
 
 public:
 
+  /** Indicator for the HDF5 file being a pandora file */
   static const std::string FORMAT;
+  /** The version of the pandora format */
   static const std::string VERSION;
 
+  /** Read only file open mode */
   static const size_t READ_ONLY;
+  /** Read/write file open mode */
   static const size_t READ_WRITE;
+  /** Overwrite file open mode */
   static const size_t OVERWRITE;
 
+  /**
+   * Constructor that is used to open the file.
+   *
+   * @param name    The name of the file to open.
+   * @param prefix  The prefix used for IDs.
+   * @param mode    File open mode READ_ONLY, READ_WRITE or OVERWRITE.
+   */
   File(std::string name, std::string prefix, int mode = READ_WRITE);
 
+  /**
+   * Copy constructor.
+   *
+   * @param other   The file to copy.
+   */
   File(const File &other);
 
-  bool fileExists(std::string name) const;
-
+  /**
+   * Get the number of blocks in in the file.
+   *
+   * @return The number of blocks.
+   */
   size_t blockCount() const;
 
+  /**
+   * Check if a block exists in the file.
+   *
+   * @param id    The ID of the block.
+   *
+   * @return True if the block exists, false otherwise.
+   */
   bool hasBlock(std::string id) const;
 
+  /**
+   * Read an existing block from the file.
+   *
+   * @param id    The ID of the block.
+   *
+   * @return The block with the given id.
+   */
   Block getBlock(std::string id) const;
 
+  /**
+   * Read an existing with block from the file, addressed by index.
+   *
+   * @param index   The index of the block to read.
+   *
+   * @return The block at the given index.
+   */
   Block getBlock(size_t index) const;
 
+  /**
+   * Create an new block, that is immediately persisted in the file.
+   *
+   * @param name    The name of the block.
+   * @param type    The type of the block.
+   *
+   * @return The created block.
+   */
   Block createBlock(std::string name, std::string type);
 
-  void deleteBlock(std::string block_id);
+  /**
+   * Delete a block from the file.
+   *
+   * @param id    The id of the block to delete.
+   */
+  void deleteBlock(std::string id);
 
-  // std::string blockId(size_t index) const;
-
-  // std::string blockName(size_t index);
-
-  /// @todo Iterate by name
+  // @todo Iterate by name
   //std::iterator<Block> blocks() const;
-
 
   // Section getSection(std::string section_id) const;
 
@@ -64,32 +130,70 @@ public:
 
   // Section deleteSection(std::string section_id) const;
 
-  // Section deleteSection(Section &section) const;
-
+  /**
+   * Create an id with the prefix used by the file.
+   * @deprecated.
+   *
+   * @return A new id.
+   */
   std::string createId() const;
 
+  /**
+   * Read the pandora version from the file.
+   *
+   * @return The verion of the pandora file.
+   */
   std::string version() const;
 
+  /**
+   * Read the format hint from the file.
+   *
+   * @return The format hint.
+   */
   std::string format() const;
 
+  /**
+   * Get the creation date of the file.
+   *
+   * @return The creation date of the file.
+   */
   time_t createdAt() const;
 
+  /**
+   * Get the date of the last update.
+   *
+   * @return The date of the last update.
+   */
   time_t updatedAt() const;
 
-  H5::H5File getH5File() const;
 
-  void close();
+//  H5::H5File getH5File() const;
+//
+//  void close();
+//
+//  File& operator=(const File &other);
 
-  File& operator=(const File &other);
-
+  /**
+   * Comparator.
+   */
   bool operator==(const File &other) const;
 
+  /**
+   * Comparator.
+   */
   bool operator!=(const File &other) const;
 
+  /**
+   * Destructor.
+   */
   virtual ~File();
 
 private:
 
+  // check for existence
+  bool fileExists(std::string name) const;
+
+  // check if the header of the file is valid
   bool checkHeader();
 
 };
--- src/File.cpp
+++ src/File.cpp
@@ -1,4 +1,16 @@
-#include <exception>
+// Copyright (c) 2013, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+/**
+ * @file File.cpp
+ * @brief Implementation of all methods from the class File.
+ */
+
 #include <fstream>
 
 #include <hdf5.h>
@@ -12,13 +24,16 @@ using namespace std;
 
 namespace pandora {
 
+// Format definition
 const string File::VERSION = "1.0";
 const string File::FORMAT  = "pandora";
 
+// File open modes
 const size_t File::READ_ONLY  = H5F_ACC_RDONLY;
 const size_t File::READ_WRITE = H5F_ACC_RDWR;
 const size_t File::OVERWRITE  = H5F_ACC_TRUNC;
 
+/*SEE: File.hpp*/
 File::File(string name, string prefix, int mode)
   : prefix(prefix)
 {
@@ -37,12 +52,14 @@ File::File(string name, string prefix, int mode)
   }
 }
 
+/*SEE: File.hpp*/
 File::File( const File &file )
   : prefix(file.prefix), h5file(file.h5file), metadata(file.metadata), data(file.data)
 {
   // nothing to do
 }
 
+/*SEE: File.hpp*/
 bool File::fileExists(string name) const {
   ifstream f(name.c_str());
   if (f) {
@@ -53,14 +70,17 @@ bool File::fileExists(string name) const {
   }
 }
 
+/*SEE: File.hpp*/
 bool File::hasBlock(std::string id) const {
   return data.hasGroup(id);
 }
 
+/*SEE: File.hpp*/
 Block File::getBlock(std::string id) const {
   return Block(*this, data.openGroup(id, false), id);
 }
 
+/*SEE: File.hpp*/
 Block File::createBlock(string name, string type) {
   string id = util::createId("block");
   while(data.hasObject(id))
@@ -71,137 +91,24 @@ Block File::createBlock(string name, string type) {
   return b;
 }
 
+/*SEE: File.hpp*/
 void File::deleteBlock(std::string id) {
   if (data.hasGroup(id)) {
     data.delGroup(id);
   }
 }
 
+/*SEE: File.hpp*/
 size_t File::blockCount() const {
   return data.objectCount();
 }
 
+/*SEE: File.hpp*/
 Block File::getBlock(size_t index) const {
   string id = data.objectName(index);
   Block b(*this, data.openGroup(id), id);
   return b;
 }
-
-//
-//size_t File::blockCount() const {
-//  Group g = h5group.openGroup("/data");
-//  return g.getNumObjs();;
-//}
-//
-//std::string File::blockId(size_t index) const{
-//  if(index >= 0 && index < (size_t)blockCount()){
-//    Group g = h5group.openGroup("/data");
-//    std::string name;
-//    name = g.getObjnameByIdx(index);
-//    return name;
-//  }
-//  throw new runtime_error("Error in File::blockId: Index exceeds number of blocks!");
-//}
-//
-//std::string File::blockName( size_t index ) {
-//  try{
-//    Block b = getBlock(index);
-//    return b.name();
-//  }
-//  catch (Exception e) {
-//    throw new runtime_error("Error in File::blockName: Index exceeds number of blocks!");
-//  }
-//}
-//
-//Block File::getBlock( std::string block_id ){
-//  if (hasBlock(block_id)) {
-//    std::string s("/data/");
-//    s.append(block_id);
-//    Group g = this->openGroup(s);
-//    Block b(*this, block_id, g);
-//    return b;
-//  }
-//  throw "Error in File::getBlock: File does not contain block with the specified id!";
-//}
-//
-//Block File::getBlock( size_t index )  {
-//  if(index >= 0 && index < (size_t)blockCount()){
-//    std::string id = blockId(index);
-//    return getBlock(id);
-//  }
-//  throw new runtime_error("Error in File::getBlock: Index exceeds number of blocks!");
-//}
-//
-//bool File::hasBlock( std::string block_id ) const {
-//  if (block_id.length() == 0) {
-//    return false;
-//  }
-//  std::string s("/data/");
-//  s.append(block_id);
-//  return this->hasGroup(s);
-//}
-//
-//Block File::createBlock( std::string name, std::string type ) {
-//  Group g = h5group.openGroup("/data");
-//  std::string id= createId();
-//  while (hasBlock(id)){
-//    id = createId();
-//  }
-//  Group blockGroup = g.createGroup(id, 0);
-//  Block b(*this, id, blockGroup);
-//  b.type(type);
-//  b.name(name);
-//  return b;
-//}
-//
-//void File::deleteBlock( std::string block_id ) {
-//  std::string s = "/data/";
-//  s.append(block_id);
-//  this->delGroup(s);
-//}
-//
-// void File::deleteBlock( Block &block ) {
-//   std::string s = "/data/";
-//   s.append(block.blockId());
-//   this->delGroup(s);
-// }
-//
-
-//
-//void File::checkAttributes() {
-//  vector<pair<string, string> > attribs;
-//  attribs.push_back(pair<string, string> ("format", FORMAT));
-//  attribs.push_back(pair<string, string> ("version", VERSION));
-//  attribs.push_back(pair<string, string> ("created_at", time_stamp()));
-//  attribs.push_back(pair<string, string> ("updated_at", time_stamp()));
-//  for (int i = 0; i < (int) attribs.size(); i++) {
-//    if (!this->attrExists(attribs[i].first)) {
-//      this->setAttr(attribs[i].first, attribs[i].second);
-//    }
-//  }
-//}
-//
-//
-//
-//H5::H5File File::getH5File() const {
-//  return this->h5file;
-//}
-//
-//
-//string File::time_stamp() const {
-//  using namespace boost::posix_time;
-//  time_t t = time(NULL);
-//  ptime timetmp = from_time_t(t);
-//  string time_str = to_iso_string(timetmp);
-//  return time_str;
-//}
-//
-
-//void File::close() {
-//  this->setAttr("updated_at", time_stamp());
-//  h5file.close();
-//}
-
 
 /*SEE: File.hpp*/
 time_t File::updatedAt() const {
