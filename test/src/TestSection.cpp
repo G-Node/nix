26e6200f73163c0af67851bd2692a2f87c58ff2f
Jan Grewe <jan.grewe@g-node.org>
2013-04-05 17:50:59 +0200

    resolved merge problems

--- test/src/TestSection.cpp
+++ test/src/TestSection.cpp
@@ -22,7 +22,6 @@
 using namespace std;
 using namespace pandora;
 
-<<<<<<< HEAD
 class TestSection: public CPPUNIT_NS::TestFixture {
 private:
 
@@ -34,17 +33,6 @@ private:
   CPPUNIT_TEST_SUITE_END ();
 
   File *f1;
-=======
-class TestSection:public CPPUNIT_NS::TestFixture {
-private:
-
-CPPUNIT_TEST_SUITE(TestSection);
-CPPUNIT_TEST(testSectionAccess);
-    //CPPUNIT_TEST(testVersion);
-CPPUNIT_TEST_SUITE_END ();
-
-File *f1;
->>>>>>> 5151f8d... new Test for sections
 
 public:
 
@@ -52,20 +40,11 @@ public:
     f1 = new File("test_block.h5", "org.g-node", File::READ_WRITE);
   }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 5151f8d... new Test for sections
   void tearDown() {
     delete f1;
   }
 
-<<<<<<< HEAD
-  void testSectionAccess() {
-<<<<<<< HEAD
-=======
   void testAddAndRemove() {
->>>>>>> 93b4213... enum type in pandora.hpp for dataTypes.
     const char *names[5] = { "section_a", "section_b", "section_c", "section_d",
         "section_e" };
     std::string lastSectionId;
@@ -220,82 +199,6 @@ public:
 };
 
 int main(int argc, char* argv[]) {
-=======
-     const char *names[5] = {"section_a", "section_b", "section_c", "section_d", "section_e"};
-     std::string lastSectionId;
-     for (int i = 0; i < 5; i++) {
-       Section s1(f1->createSection(names[i], "Recording"));
-       Section s2(f1->getSection(s1.id()));
-       lastSectionId = s2.id();
-       stringstream errmsg;
-       errmsg << "Error while accessing block: s1.id() = " << s1.id() << " / s2.id() = " << s2.id();
-       CPPUNIT_ASSERT_MESSAGE(errmsg.str(), s1 == s2);
-     }
-     Section test = f1->getSection(lastSectionId);
-     stringstream errmsg;
-     errmsg << "Error while accessing Section with id: " << lastSectionId << ", retrieved section id = " << test.id();
-     CPPUNIT_ASSERT_MESSAGE(errmsg.str(), test.id() == lastSectionId);
-
-     CPPUNIT_ASSERT_THROW(*test.children().end(), std::range_error);
-
-     stringstream msg1;
-     msg1 << "Error while getting section iterator from section with no children: " << lastSectionId;
-     SectionIterator iter = test.children();
-     CPPUNIT_ASSERT_MESSAGE(msg1.str(), test.children() == test.children().end());
-     size_t sectionCount = f1->metadataGroup().objectCount(); // we will need that later
-     stringstream msg2;
-     msg2 << "Error while adding child sections to section: " << lastSectionId << " should have two children!";
-     Section c1 = test.addSection("child1","dataset");
-     Section c2 = test.addSection("child2","dataset");
-     size_t childCount = test.childCount();
-     CPPUNIT_ASSERT_MESSAGE(msg2.str(), childCount == 2);
-     CPPUNIT_ASSERT_MESSAGE(msg2.str(), test.hasChildren());
-
-     stringstream msg5;
-     msg5 << "Error while removing a parent section: " << test.id() << " with cascade == false!";
-     bool result = f1->removeSection(test.id(),false);
-
-     CPPUNIT_ASSERT_MESSAGE(msg5.str(), !result);
-     CPPUNIT_ASSERT_MESSAGE(msg5.str(), f1->getSection(test.id()) == test);
-
-     stringstream msg3;
-     msg3 << "Error while removing child section: " << c1.id() << " from section: " << lastSectionId << " with cascade == true!";
-     test.removeSection(c1.id(),true);
-     CPPUNIT_ASSERT_MESSAGE(msg3.str(), (sectionCount + 1) == f1->metadataGroup().objectCount());
-
-     stringstream msg4;
-     msg4 << "Error while removing child section: " << c1.id() << " from section: " << lastSectionId << " with cascade == false!";
-     test.removeSection(c2.id(),false);
-     CPPUNIT_ASSERT_MESSAGE(msg4.str(), sectionCount == (f1->metadataGroup().objectCount()));
-
-     test.addSection("child3","dataset");
-     test.addSection("child4","dataset");
-     stringstream msg6;
-     msg6 << "Error while removing section recursively: " << test.id();
-     f1->removeSection(test.id(),true);
-     CPPUNIT_ASSERT_MESSAGE(msg6.str(), (sectionCount-1) == (f1->metadataGroup().objectCount()));
-
-     /*
-     for(SectionIterator iter = f1->sections(); iter != iter.end(); ++iter){
-       Section s = *iter;
-       cout << "rootSection: " << s.id() << endl;
-       for(SectionIterator iter2 = s.children(); iter2 != iter2.end(); ++iter2){
-         Section s3 = *iter2;
-         cout << "\t\t child section: " << s3.id() << endl;
-       }
-     }
-      */
-
-   }
-
-
- };
-
-
-
-
-int main( int argc, char* argv[] ) {
->>>>>>> 5151f8d... new Test for sections
   CPPUNIT_TEST_SUITE_REGISTRATION(TestSection);
   // Informiert Test-Listener ueber Testresultate
   CPPUNIT_NS::TestResult testresult;
