4d3742623bf8ffabd75d244a87b748727d36cc69
Jan Grewe <jan.grewe@g-node.org>
2013-04-09 18:01:39 +0200

    new Value files

---
+++ include/pandora/ValueIterator.hpp
@@ -0,0 +1,40 @@
+#ifndef PAN_VALUEITERATOR_H_INCLUDED
+#define PAN_VALUEITERATOR_H_INCLUDED
+
+#include <iterator>
+#include <stdexcept>
+
+#include <pandora/Property.hpp>
+#include <pandora/Value.hpp>
+
+namespace pandora {
+
+class ValueIterator: public std::iterator<std::input_iterator_tag, Value> {
+
+protected:
+
+  Property property;
+  Group group;
+  size_t index, size;
+
+public:
+  ValueIterator(Property &property, Group group);
+  ValueIterator(const ValueIterator &other);
+
+  Value operator*() const;
+  ValueIterator &operator++();
+
+  ValueIterator begin() const;
+  ValueIterator end() const;
+
+  void operator=(const ValueIterator &other);
+
+  bool operator==(const ValueIterator &other) const;
+  bool operator!=(const ValueIterator &other) const;
+
+  virtual ~ValueIterator() {
+  }
+};
+
+} /* namespace pandora */
+#endif /* PAN_VALUE_H_INCLUDED */
---
+++ src/ValueIterator.cpp
@@ -0,0 +1,61 @@
+#include <pandora/ValueIterator.hpp>
+
+using namespace std;
+
+namespace pandora {
+
+ValueIterator::ValueIterator(Property &property, Group group) :
+    property(property), group(group) {
+  index = 0;
+  size = group.objectCount();
+}
+
+ValueIterator::ValueIterator(const ValueIterator &other) :
+    property(other.property), group(other.group), index(other.index), size(other.size) {
+}
+
+ValueIterator &ValueIterator::operator++() {
+  index++;
+  return *this;
+}
+
+ValueIterator ValueIterator::begin() const {
+  ValueIterator iter(*this);
+  iter.index = 0;
+  return iter;
+}
+
+ValueIterator ValueIterator::end() const {
+  ValueIterator iter(*this);
+  iter.index = size;
+  return iter;
+}
+
+Value ValueIterator::operator*() const {
+  string id;
+  if (index < size) {
+    id = group.objectName(index);
+  } else {
+    throw std::range_error(
+        "Attempt to access an element that is out of range!");
+  }
+  Value value(property, group.openGroup(id, false), id);
+  return value;
+}
+
+void ValueIterator::operator=(const ValueIterator &other) {
+  property = other.property;
+  group = other.group;
+  index = other.index;
+  size = other.size;
+}
+
+bool ValueIterator::operator==(const ValueIterator &other) const {
+  return property.id() == other.property.id() && group == other.group && index == other.index;
+}
+
+bool ValueIterator::operator!=(const ValueIterator &other) const {
+  return property.id() != other.property.id() || group != other.group || index != other.index;
+}
+
+} /* namespace pandora */
