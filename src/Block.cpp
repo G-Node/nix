41c1088a471805c05b173bb0037a7a22489a859b
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-05-29 18:27:36 +0200

    Block: block now inherits from

--- src/Block.cpp
+++ src/Block.cpp
@@ -1,73 +1,20 @@
+
 #include <pandora/Block.hpp>
-#include <pandora/Util.hpp>
 
 using namespace std;
 
 namespace pandora {
 
-Block::Block(const Block &block)
-    : file(block.file), group(block.group), block_id(block.block_id)
-{
-  // nothing to do
-}
 
-Block::Block(File *file, Group group, string id)
-    : file(file), group(group), block_id(id)
-{
-  // nothing to do
-}
+/* SEE: SimpleTag.hpp */
+Block::Block(const Block &block) :
+    PandoraEntity(block.file, block.group, block.entity_id)
+{}
 
-string Block::id() const{
- return block_id;
-}
-
-void Block::type(string type) {
-  group.setAttr("type", type);
-}
-
-string Block::type() const {
-  string type;
-  group.getAttr("type", type);
-  return type;
-}
-
-void Block::name(string name) {
-  group.setAttr("name", name);
-}
-
-string Block::name() const {
-  string name;
-  group.getAttr("name", name);
-  return name;
-}
-
-void Block::definition(string definition) {
-  group.setAttr("definition", definition);
-}
-
-string Block::definition() const {
-  string definition;
-  group.getAttr("definition", definition);
-  return definition;
-}
-
-void Block::date(time_t date) {
-  group.setAttr("date", util::timeToStr(date));
-}
-
-time_t Block::date() const {
-  string t;
-  group.getAttr("date", t);
-  return util::strToTime(t);
-}
-
-bool Block::operator==(const Block &other) const {
-  return block_id ==  other.block_id;
-}
-
-bool Block::operator!=(const Block &other) const {
-  return block_id != other.block_id;
-}
+/* SEE: SimpleTag.hpp */
+Block::Block(File *file, Group group, std::string id) :
+    PandoraEntity(file, group, id)
+{}
 
 Block::~Block() {
   //dtor
