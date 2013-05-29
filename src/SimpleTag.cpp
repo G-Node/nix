574d47514b4deb412afb6ff7cc8667347afaf292
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-05-29 17:34:57 +0200

    SimpleTag: first draft of simple tag

---
+++ src/SimpleTag.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2013, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+/**
+ * @file SimpleTag.cpp
+ * @brief Implementations of the class SimpleTag.
+ */
+
+
+#include <pandora/SimpleTag.hpp>
+
+namespace pandora {
+
+/* SEE: SimpleTag.hpp */
+SimpleTag::SimpleTag(const SimpleTag &tag) :
+    PandoraEntity(tag.file, tag.group, tag.entity_id)
+{}
+
+/* SEE: SimpleTag.hpp */
+SimpleTag::SimpleTag(File *file, Group group, std::string id) :
+    PandoraEntity(file, group, id)
+{}
+
+/* SEE: SimpleTag.hpp */
+SimpleTag& SimpleTag::operator=(const SimpleTag &tag) {
+  file = tag.file;
+  group = tag.group;
+  entity_id = tag.entity_id;
+  return *this;
+}
+
+SimpleTag::~SimpleTag()
+{
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace pandora */
