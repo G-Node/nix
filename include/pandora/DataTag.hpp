b4f02ab0293f5f28ab428c9f1255ef671929e695
Jan Grewe <jan.grewe@g-node.org>
2013-09-29 13:05:57 +0200

    first implementation of DataTag

---
+++ include/pandora/DataTag.hpp
@@ -0,0 +1,230 @@
+// Copyright (c) 2013, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+/**
+ * @file pandora/DataTag.hpp
+ * @brief Definition of the class DataTag.
+ */
+
+#include <pandora/PSize.hpp>
+#include <pandora/EntityWithSources.hpp>
+#include <pandora/util/ReferenceList.hpp>
+
+#ifndef PAN_DATATAG_H_INCLUDED
+#define PAN_DATATAG_H_INCLUDED
+
+namespace pandora {
+
+class Section;
+class DataArray;
+class Representation;
+class RepresentationType;
+
+enum class LinkType;
+
+/**
+ * Class that represents a pandora tag.
+ *
+ * TODO implement methods for representation.
+ * TODO implement methods for references.
+ */
+class DataTag : public EntityWithSources {
+
+private:
+
+  static const PSize MIN_CHUNK_SIZE;
+  static const PSize MAX_SIZE_1D;
+
+  Group representation_group;
+  util::ReferenceList reference_list;
+public:
+
+  /**
+   * Copy constructor
+   */
+  DataTag(const DataTag &tag);
+
+  /**
+   * Standard constructor
+   */
+  DataTag(File file, Block block, Group group, std::string id);
+
+  /**
+   * Standard constructor that preserves the creation time.
+   */
+  DataTag(File file, Block block, Group group, std::string id, time_t time);
+
+  /**
+   * Getter for the positions of a tag. The positions are strored in a DataArray.
+   *
+   *
+   * @return The positions DataArray.
+   */
+  DataArray positions() const;
+
+  /**
+   * Setter for the positions of a tag.
+   *
+   * @param position   string.
+   */
+  void positions(const std::string &positionid);
+
+  /**
+   * Setter for the positions of a tag.
+   *
+   * @param position   DataArray.
+   */
+  void positions(const DataArray &positions);
+
+  /**
+   * Getter for the extents of the tag which are stored in a#
+   * DataArray
+   *
+   * @return The extents of the tag.
+   */
+  DataArray extents() const;
+
+  /**
+   * Sets the extents DataArray of the tag.
+   *
+   * @param extent      The extent vector.
+   */
+  void extents(const DataArray &extents);
+
+  /**
+   * Set the reference to the extents DataArray.
+   *
+   * @param the id.
+   */
+  void extents(const std::string &extentsId);
+
+  //--------------------------------------------------
+  // Methods concerning references.
+  // TODO implement when done with the DataArray class.
+  // TODO add hasXy getXy addXy and removeXy methods for references.
+  //--------------------------------------------------
+
+  bool hasReference(const DataArray &reference) const;
+
+  bool hasReference(const std::string &id) const;
+
+  size_t referenceCount() const;
+
+  DataArray getReference(const std::string &id) const;
+
+  void addReference(const DataArray &reference);
+
+  bool removeReference(const DataArray &reference);
+
+  /**
+   * Getter for all referenced DataArrays.
+   *
+   * @return All referenced DataArrays
+   */
+  std::vector<DataArray> references() const;
+
+  /**
+   * Setter for all referenced DataArrays. Previously referenced
+   * DataArrays, that are not in the references vector will be
+   * removed.
+   *
+   * @param references    All referenced arrays.
+   */
+  void references(const std::vector<DataArray> &references);
+
+  //--------------------------------------------------
+  // Methods concerning representations.
+  //--------------------------------------------------
+
+  /**
+   * Checks if a specific representation exists on the tag.
+   *
+   * @param id        The id of a representation.
+   *
+   * @return True if the representation exists, false otherwise.
+   */
+  bool hasRepresentation(std::string id) const;
+
+  /**
+   * Returns the number of representations in this block.
+   *
+   * @return The number of representations.
+   */
+  size_t representationCount() const;
+
+  /**
+   * Retrieves a specific representation from the tag.
+   *
+   * @param id        The id of the representation.
+   *
+   * @return The representation with the specified id. If it doesn't exist
+   *         an exception will be thrown.
+   */
+  Representation getRepresentation(std::string id) const;
+
+  /**
+   * Retrieves a specific representation from the tag.
+   *
+   * @param index        The index of the representation.
+   *
+   * @return The representation with the specified index.
+   */
+  Representation getRepresentation(size_t index) const;
+
+  /**
+   * Getter for all representations of the tag.
+   *
+   * @return All representations as vector.
+   */
+  std::vector<Representation> representations() const;
+
+  /**
+   * Create a new representation.
+   *
+   * @param data      The data array of this representation.
+   * @param type      The link type of this representation.
+   *
+   * @return The created representation object.
+   */
+  Representation createRepresentation(DataArray data, RepresentationType link_type);
+
+  /**
+   * Remove a representation from the tag.
+   *
+   * @param id        The id of the representation to remove.
+   *
+   * @return True if the representation was removed, false otherwise.
+   */
+  bool removeRepresentation(std::string id);
+
+
+  //--------------------------------------------------
+  // Other methods and functions
+  //--------------------------------------------------
+
+  /**
+   * Assignment operator
+   */
+  DataTag& operator=(const DataTag &other);
+
+  /**
+   * Output operator
+   */
+  friend std::ostream& operator<<(std::ostream &out, const DataTag &ent);
+
+  /**
+   * Destructor.
+   */
+  virtual ~DataTag();
+
+};
+
+
+} /* namespace pandora */
+
+#endif /* PAN_DATATAG_H_INCLUDED */
---
+++ src/DataTag.cpp
@@ -0,0 +1,189 @@
+// Copyright (c) 2013, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+/**
+ * @file DataTag.cpp
+ * @brief Implementations of the class DataTag.
+ */
+
+#include <pandora/Group.hpp>
+#include <pandora/File.hpp>
+#include <pandora/Block.hpp>
+#include <pandora/DataSet.hpp>
+#include <pandora/DataTag.hpp>
+#include <pandora/DataArray.hpp>
+#include <pandora/Representation.hpp>
+
+using namespace std;
+
+namespace pandora {
+
+
+const PSize DataTag::MIN_CHUNK_SIZE = {1};
+const PSize DataTag::MAX_SIZE_1D = {H5S_UNLIMITED};
+
+
+DataTag::DataTag(const DataTag &tag)
+: EntityWithSources(tag.file, tag.block, tag.group, tag.entity_id),reference_list(tag.reference_list)
+{
+  representation_group = tag.representation_group;
+}
+
+
+DataTag::DataTag(File file, const Block block, Group group, std::string id)
+: EntityWithSources(file, block, group, id), reference_list(group, "references")
+{
+  representation_group = group.openGroup("representations");
+}
+
+
+DataTag::DataTag(File file, const Block block, Group group, std::string id, time_t time)
+: EntityWithSources(file, block, group, id, time),reference_list(group, "references")
+{
+  representation_group = group.openGroup("representations");
+}
+
+
+DataArray DataTag::positions() const {
+  std::string posId;
+  group.getAttr("positions", posId);
+  if(posId.length() > 0){
+    if(this->block.hasDataArray(posId))
+      return this->block.getDataArray(posId);
+  }
+  throw runtime_error("Unable to find DataArray with id " + posId + "!");
+}
+
+
+void DataTag::positions(const DataArray &positions) {
+  this->positions(positions.id());
+}
+
+
+void DataTag::positions(const string &positionsId) {
+  if(this->block.hasDataArray(positionsId)){
+    group.setAttr("positions", positionsId);
+    forceUpdatedAt();
+  }
+  else{
+    throw runtime_error("DataTag::positions: cannot set DataArray because it does not exist!");
+  }
+}
+
+
+DataArray DataTag::extents() const {
+  std::string extId;
+  group.getAttr("extents", extId);
+  if(extId.length() > 0){
+    if(this->block.hasDataArray(extId))
+      return this->block.getDataArray(extId);
+  }
+  throw runtime_error("Unable to find DataArray with id " + extId + "!");
+}
+
+
+void DataTag::extents(const DataArray &extent) {
+  extents(extent.id());
+}
+
+
+void DataTag::extents(const string &extentsId) {
+  if(this->block.hasDataArray(extentsId)){
+    group.setAttr("extents", extentsId);
+    forceUpdatedAt();
+  }
+  else{
+    throw runtime_error("DataTag::extents: cannot set DataArray because it does not exist!");
+  }
+}
+
+// Methods concerning references.
+
+bool DataTag::hasReference(const DataArray &reference) const {
+  return hasReference(reference.id());
+}
+
+bool DataTag::hasReference(const std::string &id) const {
+  return reference_list.has(id);
+}
+
+size_t DataTag::referenceCount() const {
+  return reference_list.count();
+}
+
+DataArray DataTag::getReference(const std::string &id) const {
+  if (hasReference(id)) {
+    return block.getDataArray(id);
+  } else {
+    throw runtime_error("No reference with id: " + id);
+  }
+}
+
+void DataTag::addReference(const DataArray &reference) {
+  reference_list.add(reference);
+}
+
+bool DataTag::removeReference(const DataArray &reference) {
+  return reference_list.remove(reference);
+}
+
+std::vector<DataArray> DataTag::references() const {
+  vector<string> refs = reference_list.get();
+  vector<DataArray> data_obj;
+
+  for (size_t i = 0; i < refs.size(); i++) {
+    if (block.hasDataArray(refs[i])) {
+      data_obj.push_back(block.getDataArray(refs[i]));
+    } else {
+      // TODO ???
+    }
+  }
+  return data_obj;
+}
+
+void DataTag::references(const std::vector<DataArray> &references) {
+  vector<string> ids(references.size());
+
+  for (size_t i = 0; i < references.size(); i++) {
+    ids[i] = references[i].id();
+  }
+
+  reference_list.set(ids);
+}
+
+// Other methods and functions
+
+DataTag& DataTag::operator=(const DataTag &other) {
+  if (*this != other) {
+    this->file = other.file;
+    this->block = other.block;
+    this->group = other.group;
+    this->entity_id = other.entity_id;
+    this->representation_group = other.representation_group;
+    this->sources_refs = other.sources_refs;
+    this->reference_list = other.reference_list;
+  }
+  return *this;
+}
+
+
+ostream& operator<<(ostream &out, const DataTag &ent) {
+  out << "DataTag: {name = " << ent.name();
+  out << ", type = " << ent.type();
+  out << ", id = " << ent.id() << "}";
+  return out;
+}
+
+
+DataTag::~DataTag()
+{
+  // TODO Auto-generated destructor stub
+}
+
+
+} /* namespace pandora */
