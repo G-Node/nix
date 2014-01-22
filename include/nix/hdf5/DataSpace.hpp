9a4b22dbb5a349076b4d4cf2213f353802074cc3
Christian Kellner <christian@kellner.me>
2014-01-22 13:21:42 +0100

    hdf5::DataSpace: new functions to ease DataSpace creation

---
+++ include/nix/hdf5/DataSpace.hpp
@@ -0,0 +1,28 @@
+// Copyright © 2014, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+//
+// Author: Christian Kellner <kellner@bio.lmu.de>
+
+#include <nix/hdf5/hdf5include.hpp>
+#include <nix/NDSize.hpp>
+
+#ifndef NIX_DATASPACE_H
+#define NIX_DATASPACE_H
+
+namespace nix {
+namespace hdf5 {
+namespace DataSpace {
+
+H5::DataSpace create(const NDSize &dims, const NDSize *maxdims = nullptr);
+H5::DataSpace create(const NDSize &dims, bool maxdimsUnlimited);
+
+} //::nix::hdf5::DataSpace
+} //::nix::hdf5
+} //::nix
+
+#endif
