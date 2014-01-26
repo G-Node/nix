1279974ce5e51d450b5f0ed0bade9e9e54f3d40f
Christian Kellner <christian@kellner.me>
2014-01-26 16:32:29 +0100

    hdf5::DataSet: Fix datatype in read template

--- include/nix/hdf5/DataSet.hpp
+++ include/nix/hdf5/DataSet.hpp
@@ -207,7 +207,7 @@ template<typename T> void DataSet::read(T &value, const Selection &fileSel, cons
 {
     Hydra<T> hydra(value);
 
-    H5::DataType dtype = data_type_to_h5_memtype(hydra.element_data_type());
+    DataType dtype = hydra.element_data_type();
     NDSize size = hydra.shape();
     this->read(dtype, size, hydra.data(), fileSel, memSel);
 }
