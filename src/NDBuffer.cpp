a2eb414d1c9d0902db4be84d3a5f897e8edfea3e
Christian Kellner <christian@kellner.me>
2014-01-29 17:12:28 +0100

    NDBuffer: Move out of the hdf5 subdirectory

--- src/NDBuffer.cpp
+++ src/NDBuffer.cpp
@@ -7,8 +7,7 @@
 // modification, are permitted under the terms of the BSD License. See
 // LICENSE file in the root of the Project.
 
-#include <nix/hdf5/NDBuffer.hpp>
-
+#include <nix/NDBuffer.hpp>
 #include <nix/hdf5/DataTypeHDF5.hpp>
 
 namespace nix {
