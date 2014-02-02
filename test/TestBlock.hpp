c8b105e41d6bcc70ebaff31b55d403225c39d73d
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-02-02 23:11:08 +0100

    Test for nix::Block almost complete

--- test/TestBlock.hpp
+++ test/TestBlock.hpp
@@ -29,11 +29,25 @@ class TestBlock : public CPPUNIT_NS::TestFixture {
 private:
 
     CPPUNIT_TEST_SUITE(TestBlock);
+
+    CPPUNIT_TEST(testId);
+    CPPUNIT_TEST(testType);
+    CPPUNIT_TEST(testName);
+    CPPUNIT_TEST(testDefinition);
+
+    CPPUNIT_TEST(testMetadataAccess);
+    CPPUNIT_TEST(testSourceAccess);
+    CPPUNIT_TEST(testDataArrayAccess);
+    CPPUNIT_TEST(testSimpleTagAccess);
+    CPPUNIT_TEST(testDataTagAccess);
+
     CPPUNIT_TEST(testUpdatedAt);
     CPPUNIT_TEST(testCreatedAt);
+
     CPPUNIT_TEST_SUITE_END ();
 
     nix::File file;
+    nix::Section section;
     nix::Block block, block_other, block_null;
     time_t startup_time;
 
@@ -42,6 +56,18 @@ public:
     void setUp();
     void tearDown();
 
+    void testId();
+    void testType();
+    void testName();
+    void testDefinition();
+
+    void testMetadataAccess();
+    void testSourceAccess();
+    void testDataArrayAccess();
+    void testSimpleTagAccess();
+    void testDataTagAccess();
+
+    void testOperators();
     void testUpdatedAt();
     void testCreatedAt();
 
