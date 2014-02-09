a3dd0d0699c6317f71c3f2b7b45d0172e8789da4
Jan Grewe <jan.grewe@g-node.org>
2014-02-09 19:42:44 +0100

    new test for EntityWithMetadata

---
+++ test/TestEntityWithMetadata.cpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2013, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+#include <nix/util/util.hpp>
+#include "TestEntityWithMetadata.hpp"
+
+using namespace std;
+using namespace nix;
+
+
+void TestEntityWithMetadata::setUp() {
+    file = File::open("test_block.h5", FileMode::Overwrite);
+
+    section = file.createSection("foo_section", "metadata");
+
+    block = file.createBlock("block_one", "dataset");
+}
+
+
+void TestEntityWithMetadata::tearDown() {
+    file.close();
+}
+
+
+void TestEntityWithMetadata::testMetadataAccess() {
+    CPPUNIT_ASSERT(!block.hasMetadata());
+
+    block.metadata(section);
+    CPPUNIT_ASSERT(block.hasMetadata());
+
+    block.removeMetadata();
+    CPPUNIT_ASSERT(!block.hasMetadata());
+}
+
+
+
+
+
+
