4cda3b5b9f96bccc3467c8439ed7eee4e2b2ed87
Christian Kellner <christian@kellner.me>
2014-02-11 14:25:29 +0100

    Platform.hpp: new header for platform specific things
    Currently only Win32 specific things are defined here

---
+++ include/nix/Platform.hpp
@@ -0,0 +1,20 @@
+// Copyright © 2014 German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+//
+// Author: Christian Kellner <kellner@bio.lmu.de>
+
+#ifdef _WIN32
+ #ifdef NIXEXPORT
+ #define NIXAPI __declspec(dllexport)
+ #else
+ #define NIXAPI __declspec(dllimport)
+ #endif
+ #pragma warning(disable: 4250)
+#else
+ #define NIXAPI
+#endif
