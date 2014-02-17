581bb5d5477b561463054fd454de819247e95bc0
Jan Grewe <jan.grewe@g-node.org>
2014-02-17 22:24:09 +0100

    documentation of the Dimensions.

--- include/nix/Dimensions.hpp
+++ include/nix/Dimensions.hpp
@@ -20,7 +20,13 @@ class RangeDimension;
 class SetDimension;
 
 /**
- * TODO documentation
+ * Instances of the Dimension subclasses are used to define the axes of a DataArray.
+ * There are  three subclasses:
+ *     - RangeDimension
+ *     - SampledDimension
+ *     - SetDimension
+ *
+ * See detailed descriptions below.
  */
 class Dimension : public virtual base::IDimension, public base::ImplContainer<base::IDimension> {
 
@@ -66,7 +72,11 @@ public:
 
 
 /**
- * TODO documentation
+ * Instances of the SampledDimension Class are used to describe a dimension of a DataArray that
+ * has been sampled in regular intervals. For example this can be a time axis.
+ *
+ * SampledDimensions are characterized by the label of the dimension, the unit in which the sampling_interval
+ * is given. If not otherwise stated the dimension starts with zero offset.
  */
 class SampledDimension : public virtual base::ISampledDimension, public base::ImplContainer<base::ISampledDimension> {
 
@@ -80,52 +90,100 @@ public:
 
     SampledDimension(const SampledDimension &other);
 
-
+    /**
+     * Returns the id of the dimension entity which at the same time is
+     * the order of the dimension. That is the dimension of the actual data that
+     * is defined by this SampledDimension.
+     *
+     * @return size_t, the id, i.e. the order.
+     */
     size_t id() const {
         return impl_ptr->id();
     }
 
-
+    /**
+     * Returns the type of the dimension entity.
+     *
+     * @return DimensionType either Sample, Range, or Set.
+     */
     DimensionType dimensionType() const {
         return impl_ptr->dimensionType();
     }
 
-
+    /**
+     * Returns the label of the dimension.
+     *
+     * @return string the label.
+     */
     std::string label() const {
         return impl_ptr->label();
     }
 
-
+    /**
+     * Set the label of the dimension.
+     * -obligatory-
+     *
+     * @param label string
+     */
     void label(const std::string &label) {
         impl_ptr->label(label);
     }
 
-
+    /**
+     * Returns the unit in which sampling_interval and offset are given.
+     *
+     * @return string the unit
+     */
     std::string unit() const {
         return impl_ptr->unit();
     }
 
-
+    /**
+     * Sets the unit in which sampling_interval and offset are given.
+     * -obligatory-
+     *
+     * @param unit, string
+     */
     void unit(const std::string &unit) {
         impl_ptr->unit(unit);
     }
 
-
+    /**
+     * Returns the sampling interval in which the dimension has been sampled.
+     *
+     * @return double, the sampling interval
+     */
     double samplingInterval() const {
        return impl_ptr->samplingInterval();
     }
 
-
+    /**
+     * Set the sampling interval in which the dimension has been sampled.
+     * -obligatory-
+     *
+     * @param interval double
+     */
     void samplingInterval(double interval) {
         impl_ptr->samplingInterval(interval);
     }
 
-
+    /**
+     * Returns the offset of this dimension. This property is optional. Thus,
+     * this return value may be empty.
+     *
+     * @return boost::optional<double> the offset
+     */
     boost::optional<double> offset() const {
         return impl_ptr->offset();
     }
 
-
+    /**
+     * Set the offset of this dimension. The offset is interpreted using the same
+     * unit as the sampling interval.
+     * -optional-
+     *
+     * @param offset double
+     */
     void offset(double offset) {
         return impl_ptr->offset(offset);
     }
@@ -140,7 +198,11 @@ public:
 
 
 /**
- * TODO documentation
+ * Instances of the SetDimension Class are used to describe a dimension of a DataArray that
+ * are sampled on a nominal scale or in categories. For example, the parallel channels of a
+ * recording device can be defined with a SetDimension.
+ *
+ * SetDimensions are characterized by a set of labels.
  */
 class SetDimension : virtual public base::ISetDimension, public base::ImplContainer<base::ISetDimension> {
 
@@ -155,21 +217,41 @@ public:
     SetDimension(const SetDimension &other);
 
 
+    /**
+    * Returns the id of the dimension entity which at the same time is
+    * the order of the dimension. That is the dimension of the actual data that
+    * is defined by this SetDimension.
+    *
+    * @return size_t, the id, i.e. the order.
+    */
     size_t id() const {
         return impl_ptr->id();
     }
 
-
+    /**
+     * Returns the type of the dimension entity.
+     *
+     * @return DimensionType either Sample, Range, or Set.
+     */
     DimensionType dimensionType() const {
         return impl_ptr->dimensionType();
     }
 
-
+    /**
+     * Returns the set of labels stored in this SetDimension.
+     *
+     * @return vector<string> the labels
+     */
     std::vector<std::string> labels() const {
         return impl_ptr->labels();
     }
 
-
+    /**
+     * Sets the labels stored in this SetDimension.
+     * -obligatory-
+     *
+     * @param labels, vector<string>
+     */
     void labels(const std::vector<std::string> &labels) {
         impl_ptr->labels(labels);
     }
@@ -184,7 +266,12 @@ public:
 
 
 /**
- * TODO documentation
+ * Instances of the RangeDimension Class are used to describe a dimension of a DataArray that
+ * are sampled in irregular intervals. For example, irregularly occurring events that have been
+ * detected in a recorded signal.
+ *
+ * RangeDimensions are characterized by an array of tics that are given in a certain unit and
+ * must be given in an ascending order. The dimension has also a label.
  */
 class RangeDimension : virtual public base::IRangeDimension, public base::ImplContainer<base::IRangeDimension> {
 
@@ -198,42 +285,81 @@ public:
 
     RangeDimension(const RangeDimension &other);
 
-
+    /**
+     * Returns the id of the dimension entity which, at the same time, is
+     * the order of the dimension. That is the dimension of the actual data that
+     * is defined by this RangeDimension.
+     *
+     * @return size_t, the id, i.e. the order.
+     */
     size_t id() const {
         return impl_ptr->id();
     }
 
-
+    /**
+     * Returns the type of the dimension entity.
+     *
+     * @return DimensionType either Sample, Range, or Set.
+     */
     DimensionType dimensionType() const {
         return impl_ptr->dimensionType();
     }
 
-
+    /**
+     * Returns the label stored in this dimension.
+     *
+     * @return string the label
+     */
     std::string label() const {
         return impl_ptr->label();
     }
 
-
+    /**
+     * Sets the label of the dimension.
+     * -obligatory-
+     *
+     * @param label string
+     */
     void label(const std::string &label) {
         impl_ptr->label(label);
     }
 
-
+    /**
+     * Returns the unit of this RangeDimension.
+     *
+     * @return string the unit.
+     */
     std::string unit() const {
         return impl_ptr->unit();
     }
 
-
+    /**
+     * Sets the unit of this RangeDimension. So far, only SI units are
+     * permitted.
+     * -obligatory-
+     *
+     * @param unit string
+     */
     void unit(const std::string &unit) {
         impl_ptr->unit(unit);
     }
 
-
+    /**
+     * Retruns the set of tics stored in this RangeDimension.
+     *
+     * @return vector<double> the tics.
+     */
     std::vector<double> tics() const {
         return impl_ptr->tics();
     }
 
-
+    /**
+     * Sets the tics of the RangeDimension. Tics must be ordered
+     * in ascending order.
+     * -obligatory-
+     *
+     * @param tics vector<double>
+     */
     void tics(const std::vector<double> &tics) {
         impl_ptr->tics(tics);
     }
