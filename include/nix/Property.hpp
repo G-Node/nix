6b50175f07bce48e3b76a53123101de5d5d175a5
Jan Grewe <jan.grewe@g-node.org>
2014-03-07 23:02:57 +0100

    Docstrings in Property.hpp

--- include/nix/Property.hpp
+++ include/nix/Property.hpp
@@ -54,37 +54,77 @@ public:
     }
     */
 
-
+    /**
+     * Set the mapping information for this Property. The mapping defines how
+     * this Property should be treated in a mapping procedure. The mapping is provided
+     * in form of an url pointing to the definition of a section into which this
+     * property should be mapped
+     * (e.g. http:// ... /preparation/preparation.xml#preparation:BathSolution
+     *
+     * @param mapping string the mapping information.
+     *
+     */
     void mapping(const std::string &mapping) {
         backend()->mapping(mapping);
     }
 
-
+    /**
+     * Getter for the mapping information stored in this Property.
+     * The result may be not initialized, check on true before
+     * dereferencing.
+     *
+     * @returns boost::optional<String> the mapping
+     */
     boost::optional<std::string> mapping() const {
         return backend()->mapping();
     }
     
-    
+    /**
+     * Deletes the mapping information.
+     *
+     * @param boost::none_t
+     */
     void mapping(const boost::none_t t) {
         backend()->mapping();
     }
 
-
+    /**
+     * Returns the dataType of the stored Values. Returned value may be
+     * not initialized if no value present, check on true before dereferencing.
+     *
+     * @param boost::optional<nix::DataType> the value DataType
+     *
+     */
     boost::optional<DataType> dataType() const {
         return backend()->dataType();
     }
 
-
+    /**
+     * Set the unit of the stored values. Note: all values
+     * have the same unit.
+     *
+     * @param std::string the unit
+     */
     void unit(const std::string &unit) {
         backend()->unit(unit);
     }
 
-
+    /**
+     * Returns the unit. Return value may be uninitialized,
+     * check for true before dereferencing.
+     *
+     * @return boost::optional<String> the unit
+     *
+     */
     boost::optional<std::string> unit() const {
         return backend()->unit();
     }
     
-    
+    /**
+     * Deletes the unit information.
+     *
+     * @param boost::none_t
+     */
     void unit(const boost::none_t t) {
         return backend()->unit(t);
     }
@@ -93,23 +133,47 @@ public:
     // Methods for Value access
     //--------------------------------------------------
 
+    /**
+     * Deletes all values of this Property.
+     *
+     */
     void deleteValues() {
         backend()->deleteValues();
     }
 
-
+    /**
+     * Returns the number of values stored in this Property.
+     *
+     * @return size_t the count
+     */
     size_t valueCount() const {
         return backend()->valueCount();
     }
 
+    /**
+     * Set the values of this Property. Replaces all existing Values.
+     *
+     * @param std::vector<Value> the new values.
+     */
     void values(const std::vector<Value> &values) {
         backend()->values(values);
     }
 
+    /**
+     * Returns the values stored in this Property. Returned vector may
+     * be empty.
+     *
+     * @return std::vector<Value> the values.
+     */
     std::vector<Value> values(void) const {
         return backend()->values();
     }
     
+    /**
+     * Deletes the values stored in this Property.
+     *
+     * @param boost::none_t
+     */
     void values(const boost::none_t t) {
         backend()->values(t);
     }
