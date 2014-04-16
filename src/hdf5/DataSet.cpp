2eeef29513cf8202c9981e5de9ece554e4ce763f
Christian Kellner <christian@kellner.me>
2014-04-16 11:03:41 +0200

    DataSet: unify {file, mem}TypeForValue() to common function
    The common function is the new h5_type_for_value_dtype()

--- src/hdf5/DataSet.cpp
+++ src/hdf5/DataSet.cpp
@@ -367,37 +367,30 @@ H5::DataType h5_type_for_value(bool forMemory)
 #endif
 #define DATATYPE_SUPPORT_NOT_IMPLEMENTED false
 //
-
-H5::DataType DataSet::fileTypeForValue(DataType dtype)
+static H5::DataType h5_type_for_value_dtype(DataType dtype, bool forMemory)
 {
     switch(dtype) {
-    case DataType::Bool:   return h5_type_for_value<bool>(false);
-    case DataType::Int32:  return h5_type_for_value<int32_t>(false);
-    case DataType::UInt32: return h5_type_for_value<uint32_t>(false);
-    case DataType::Int64:  return h5_type_for_value<int64_t>(false);
-    case DataType::UInt64: return h5_type_for_value<uint64_t>(false);
-    case DataType::Double: return h5_type_for_value<double>(false);
-    case DataType::String: return h5_type_for_value<char *>(false);
+    case DataType::Bool:   return h5_type_for_value<bool>(forMemory);
+    case DataType::Int32:  return h5_type_for_value<int32_t>(forMemory);
+    case DataType::UInt32: return h5_type_for_value<uint32_t>(forMemory);
+    case DataType::Int64:  return h5_type_for_value<int64_t>(forMemory);
+    case DataType::UInt64: return h5_type_for_value<uint64_t>(forMemory);
+    case DataType::Double: return h5_type_for_value<double>(forMemory);
+    case DataType::String: return h5_type_for_value<char *>(forMemory);
 #ifndef CHECK_SUPOORTED_VALUES
     default: assert(DATATYPE_SUPPORT_NOT_IMPLEMENTED); return H5::DataType{};
 #endif
     }
 }
 
+H5::DataType DataSet::fileTypeForValue(DataType dtype)
+{
+    return h5_type_for_value_dtype(dtype, false);
+}
+
 H5::DataType DataSet::memTypeForValue(DataType dtype)
 {
-    switch(dtype) {
-    case DataType::Bool:   return h5_type_for_value<bool>(true);
-    case DataType::Int32:  return h5_type_for_value<int32_t>(true);
-    case DataType::UInt32: return h5_type_for_value<uint32_t>(true);
-    case DataType::Int64:  return h5_type_for_value<int64_t>(true);
-    case DataType::UInt64: return h5_type_for_value<uint64_t>(true);
-    case DataType::Double: return h5_type_for_value<double>(true);
-    case DataType::String: return h5_type_for_value<char *>(true);
-#ifndef CHECK_SUPOORTED_VALUES
-    default: assert(DATATYPE_SUPPORT_NOT_IMPLEMENTED); return H5::DataType{};
-#endif
-    }
+    return h5_type_for_value_dtype(dtype, true);
 }
 
 template<typename T>
