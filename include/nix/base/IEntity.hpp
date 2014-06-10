b94cb76c65c6d0d8d39cb47cd48872603db5a8a9
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-06-10 17:48:00 +0200

    Documentation: doc for nix::Entity

--- include/nix/base/IEntity.hpp
+++ include/nix/base/IEntity.hpp
@@ -15,57 +15,67 @@
 namespace nix {
 namespace base {
 
-
+/**
+ * @brief Interface that declares a basic features of most entities.
+ *
+ * See {@link nix::base::Entity} for more information.
+ */
 class NIXAPI IEntity {
 public:
 
     /**
-     * Getter for the id of the entity.
+     * @brief Getter for the id of the entity.
+     *
+     * The id is generated at creation time of an entity. It contains
+     * a randomly generated sequence of characters with low collision
+     * probability.
      *
      * @return The id of the entity.
      */
     virtual std::string id() const = 0;
 
     /**
-     * Gets the time of the last update.
+     * @brief Gets the time of the last update.
      *
      * @return Time of the last update.
      */
     virtual time_t updatedAt() const = 0;
 
     /**
-     * Gets the creation time.
+     * @brief Gets the creation time.
      *
      * @return The creation time of the entity.
      */
     virtual time_t createdAt() const = 0;
 
     /**
-     * Sets the time of the last update to the current time if
+     * @brief Sets the time of the last update to the current time if
      * the field is not set.
      */
     virtual void setUpdatedAt() = 0;
 
     /**
-     * Sets the time of the last update to the current time.
+     * @brief Sets the time of the last update to the current time.
      */
     virtual void forceUpdatedAt() = 0;
 
     /**
-     * Sets the creation time to the current time if the attribute
-     * created_at is not set.
+     * @brief Sets the creation time to the current time if the creation
+     * time is not set.
      */
     virtual void setCreatedAt() = 0;
 
     /**
-     * Sets the creation time to the provided value even if the
-     * attribute created_at is set.
+     * @brief Sets the creation time to the provided value even if the
+     * field is already set.
      *
      * @param time The creation time to set.
      */
     virtual void forceCreatedAt(time_t t) = 0;
 
-
+    /**
+     * @brief Destructor
+     */
     virtual ~IEntity() {}
 };
 
