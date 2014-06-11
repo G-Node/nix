48226b4b6c3629d4ff8d4a67ff926076942b5c80
Christian Kellner <christian@kellner.me>
2014-06-11 14:22:18 +0200

    [h5] RangeDimension::label: fix returning none if it is not there

--- src/hdf5/DimensionHDF5.cpp
+++ src/hdf5/DimensionHDF5.cpp
@@ -312,8 +312,10 @@ DimensionType RangeDimensionHDF5::dimensionType() const {
 boost::optional<std::string> RangeDimensionHDF5::label() const {
     boost::optional<std::string> ret;
     string label;
-    group.getAttr("label", label);
-    ret = label;
+    bool have_attr = group.getAttr("label", label);
+    if (have_attr) {
+        ret = label;
+    }
     return ret;
 }
 
