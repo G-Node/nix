1ccde7bc149f1e2c97100e4cf6b9223edd703e21
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-06-12 18:51:35 +0200

    Documentation: doc added for Section

--- include/nix/base/ISection.hpp
+++ include/nix/base/ISection.hpp
@@ -23,7 +23,11 @@ class Property;
 
 namespace base {
 
-
+/**
+ * @brief Interface for implementations of the Section entity.
+ *
+ * See {@link nix::Section} for a more detailed description.
+ */
 class NIXAPI ISection : virtual public base::INamedEntity  {
 
 public:
@@ -33,80 +37,90 @@ public:
     //--------------------------------------------------
 
     /**
-     * Set the repository in which a section of this type is defined. Usually
-     * this information is provided in the form of an URL
+     * @brief Set the repository in which a section of this type is defined.
      *
-     * @param string the url of the repository.
+     * Usually this information is provided in the form of an URL
+     *
+     * @param repository        URL to the repository.
      */
     virtual void repository(const std::string &repository) = 0;
 
     /**
-     * Returns the repository url.
+     * @brief Gets the repository URL.
      *
-     * @return boost::optional<string> the url.
+     * @return The URL to the repository.
      */
     virtual boost::optional<std::string> repository() const = 0;
 
     /**
-     * Deleter for the repository url.
+     * @brief Deleter for the repository.
      *
-     * @param boost::none_t.
+     * @param t         None
      */
     virtual void repository(const boost::none_t t) = 0;
-    
+
     /**
-     * Establish a link to another section. The linking section
-     * inherits the properties defined in the linked section.
+     * @brief Establish a link to another section.
+     *
+     * The linking section inherits the properties defined in the linked section.
      * Properties of the same name are overridden.
      *
-     * @param the id of the linked section.
+     * @param id        The id of the section that should be linked.
      */
     virtual void link(const std::string &id) = 0;
 
     /**
-     * Returns the id of the linked section.
+     * @brief Get the linked section.
      *
-     * @return string the id.
+     * @return The linked section. If no section was linked a null
+     *         Section will be returned.
      */
     virtual Section link() const = 0;
 
     /**
-     * Deleter for the reference to the linked section.
-     * NOTE: this function does not affect the linked section object.
+     * @brief Deleter for the linked section.
      *
-     * @param boost::none_t.
+     * This just removes the link between both sections, but does not remove
+     * the linked section from the file.
+     *
+     * @param t         None
      */
     virtual void link(const none_t t) = 0;
-    
+
     /**
-     * Sets the mapping information for this section.
+     * @brief Sets the mapping information for this section.
      *
-     * @param string the mapping information.
+     * The mapping is provided as a path or URL to another section.
+     *
+     * @param mapping   The mapping information to this section.
      */
     virtual void mapping(const std::string &mapping) = 0;
 
     /**
-     * Return the mapping information.
+     * @brief Gets the mapping information.
      *
-     * @return boost::optional<std::string>
+     * @return The mapping information.
      */
     virtual boost::optional<std::string> mapping() const = 0;
 
     /**
-     * Deleter for the mapping information.
+     * @brief Deleter for the mapping information.
      *
-     * @param boost::none_t.
+     * @param t         None
      */
     virtual void mapping(const none_t t) = 0;
-    
+
     //--------------------------------------------------
     // Methods for parent access
     //--------------------------------------------------
 
     /**
-     * Returns the parent information.
+     * @brief Returns the parent section.
      *
-     * @return string
+     * Each section which is not a root section has a parent.
+     *
+     * @return The parent section. If the section has no parent, a null
+     *         section will be returned.
      */
     virtual Section parent() const = 0;
 
@@ -115,55 +129,55 @@ public:
     //--------------------------------------------------
 
     /**
-     * Return the number of children in this section.
+     * @brief Get the number of child section of the section.
      *
      * @return The number of child sections.
      */
     virtual size_t sectionCount() const = 0;
 
     /**
+     * @brief Checks whether a section has a certain child section.
      *
+     * @param id        The id of requested section.
      *
-     * @param id    The id of requested section.
-     *
-     * @return bool
+     * @return True if the section is a child, false otherwise.
      */
     virtual bool hasSection(const std::string &id) const = 0;
 
     /**
-     * Get a specific child by id.
+     * @brief Get a specific child section by its id.
      *
-     * @param id    The id of the child.
+     * @param id        The id of the child section.
      *
      * @return The child section.
      */
     virtual Section getSection(const std::string &id) const = 0;
 
     /**
-     * Get a child section by its index.
+     * @brief Get a child section by its index.
      *
-     * @param index The index of the child.
+     * @param index     The index of the child.
      *
-     * @return The child section.
+     * @return The specified child section.
      */
     virtual Section getSection(size_t index) const = 0;
 
     /**
-     *  Adds a new child section.
+     *  @brief Adds a new child section.
      *
-     *  @param name: the name of the new section
-     *  @param type: the type of the section
+     *  @param name     The name of the new section
+     *  @param type     The type of the section
      *
-     *  @return the new section.
+     *  @return The new child section.
      */
     virtual Section createSection(const std::string &name, const std::string &type) = 0;
 
     /**
-     * Deletes  a subsection from this Section.
+     * @brief Deletes a section from the section.
      *
-     * @param string the id of target section.
+     * @param id        The id of the child section to delete.
      *
-     * @return bool successful or not
+     * @return True if the section was deleted, false otherwise.
      */
     virtual bool deleteSection(const std::string &id) = 0;
 
@@ -172,28 +186,32 @@ public:
     //--------------------------------------------------
 
     /**
-     * Returns the number of properties of this section.
+     * @brief Gets the number of properties of this section.
      *
      * @return The number of Properties
      */
     virtual size_t propertyCount() const = 0;
 
     /**
-     * Checks if a Property with this id exists in this Section.
+     * @brief Checks if a Property with this id exists in this Section.
      *
-     * @param string the id.
+     * @param id        The id of the property.
+     *
+     * @return True if the property exists, false otherwise.
      */
     virtual bool hasProperty(const std::string &id) const = 0;
 
     /**
-     * Returns the Property identified by id.
+     * @brief Gets the Property identified by its id.
      *
-     * @return Property
+     * @param id        The id of the property.
+     *
+     * @return The specified property.
      */
     virtual Property getProperty(const std::string &id) const = 0;
 
     /**
-     * Returns the property defined by its index.
+     * @brief Gets the property defined by its index.
      *
      * @param index     The index of the property
      *
@@ -202,7 +220,7 @@ public:
     virtual Property getProperty(size_t index) const = 0;
 
     /**
-     * Checks if a property with a certian name exists.
+     * @brief Checks if a property with a certain name exists.
      *
      * @param name      The name of the property.
      *
@@ -211,7 +229,7 @@ public:
     virtual bool hasPropertyWithName(const std::string &name) const = 0;
 
     /**
-     * Returns a property identified by its name.
+     * @brief Returns a property identified by its name.
      *
      * @param name      The name of the property.
      *
@@ -220,22 +238,27 @@ public:
     virtual Property getPropertyByName(const std::string &name) const = 0;
 
     /**
-     * Add a Property to this section.
+     * @brief Add a new Property to the Section.
      *
-     * @param string the name of the Property.
+     * @param name      The name of the property.
+     * @param type      The type of the property.
      *
-     * @return the Property
+     * @return The newly created property.
      */
     virtual Property createProperty(const std::string &name, const std::string &type) = 0;
 
     /**
-     * Delete the Property that is identified by the id.#
+     * @brief Delete the Property identified by its id.
      *
-     * @param string the id.
+     * @param id        The id of the property.
+     *
+     * @return True if the property was deleted, false otherwise.
      */
     virtual bool deleteProperty(const std::string &id) = 0;
 
-
+    /**
+     * @brief Destructor
+     */
     virtual ~ISection() {}
 
 };
