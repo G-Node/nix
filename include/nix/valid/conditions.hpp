7cf41298f557228a824c37fb79ca6b5450f19be5
Balint Morvai <balint@morvai.de>
2014-07-16 10:38:54 +0200

    Removed NIXAPI (aka dllexport) on templated functions

--- include/nix/valid/conditions.hpp
+++ include/nix/valid/conditions.hpp
@@ -41,7 +41,6 @@ namespace valid {
      * @returns {Function} The created condition check.
      */
     template<typename TOBJ, typename TBASEOBJ, typename TRET, typename TCHECK>
-	NIXAPI
     condition
     must(const TOBJ &parent, TRET(TBASEOBJ::*get)(void)const, const TCHECK &check, const std::string &msg) {
         return [parent, get, check, msg] () -> Result {
@@ -83,7 +82,6 @@ namespace valid {
      * @returns {Function} The created condition check.
      */
     template<typename TOBJ, typename TBASEOBJ, typename TRET, typename TCHECK>
-	NIXAPI
     condition
     should(const TOBJ &parent, TRET(TBASEOBJ::*get)(void)const, const TCHECK &check, const std::string &msg) {
         return [parent, get, check, msg] () -> Result {
