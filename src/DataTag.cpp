f5a49602e7bd9407aca8a060bfa0f7d03be32e3f
Balint Morvai <balint@morvai.de>
2014-07-23 15:55:19 +0200

    Removed in class validation from DataTag&splitted references getter

---
+++ src/DataTag.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2013, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+#include <nix.hpp>
+
+namespace nix {
+
+} // namespace nix
