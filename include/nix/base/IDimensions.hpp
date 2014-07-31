f15c464272d820b0d7e272546eb6e4b21168a072
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-07-31 12:59:42 +0200

    Dimensions: move comments

--- include/nix/base/IDimensions.hpp
+++ include/nix/base/IDimensions.hpp
@@ -39,29 +39,12 @@ protected:
 
 public:
 
-    /**
-     * @brief The actual dimension that is described by the dimension descriptor.
-     *
-     * The index of the dimension entity representing the dimension of the actual
-     * data that is defined by this descriptor.
-     *
-     * @return The dimension index of the dimension.
-     */
     virtual size_t index() const = 0;
 
-    /**
-     * @brief The type of the dimension.
-     *
-     * This field indicates whether the dimension is a SampledDimension, SetDimension or
-     * RangeDimension.
-     *
-     * @return The dimension type.
-     */
+
     virtual DimensionType dimensionType() const = 0;
 
-    /**
-     * @brief Destructor
-     */
+
     virtual ~IDimension() {}
 
 };
@@ -76,102 +59,39 @@ class NIXAPI ISampledDimension : virtual public IDimension {
 
 public:
 
-    /**
-     * @brief Getter for the label of the dimension.
-     *
-     * The label of a SampledDimension corresponds to the axis label
-     * in a plot of the respective dimension.
-     *
-     * @return The label of the dimension.
-     */
     virtual boost::optional<std::string> label() const = 0;
 
-    /**
-     * @brief Sets the label of the dimension.
-     *
-     * @param label     The label of the dimension.
-     */
+
     virtual void label(const std::string &label) = 0;
 
-    /**
-     * @brief Removes the label from a dimension.
-     *
-     * @param t         None
-     */
+
     virtual void label(const none_t t) = 0;
 
-    /**
-     * @brief Gets the unit of a dimension.
-     *
-     * The unit describes which SI unit applies to this dimension
-     * and to its sampling interval.
-     *
-     * @return The unit of the dimension.
-     */
+
     virtual boost::optional<std::string> unit() const = 0;
 
-    /**
-     * @brief Sets the unit of a dimension.
-     *
-     * @param unit      The unit to set.
-     */
+
     virtual void unit(const std::string &unit) = 0;
 
-    /**
-     * @brief Removes the unit from a dimension.
-     *
-     * @param t         None
-     */
+
     virtual void unit(const none_t t) = 0;
 
-    /**
-     * @brief Gets the sampling interval of the dimension.
-     *
-     * The unit of measure in which the sampling interval must be provided
-     * is defined by the {@link unit} of the SampledDimension.
-     *
-     * @return The sampling interval.
-     */
+
     virtual double samplingInterval() const = 0;
 
-    /**
-     * @brief Sets the sampling interval of the dimension.
-     *
-     * @param interval  The sampling interval to set.
-     */
+
     virtual void samplingInterval(double interval) = 0;
 
-    /**
-     * @brief Gets the offset of the dimension.
-     *
-     * The offset defines at which position the sampling was started. The offset is
-     * interpreted in the same unit as the sampling interval.
-     *
-     * By default the offset is 0.
-     *
-     * @returns The offset of the SampledDimension.
-     */
+
     virtual boost::optional<double> offset() const = 0;
 
-    /**
-     * @brief Sets the offset of the dimension.
-     *
-     * @param offset    The offset of the dimension.
-     */
+
     virtual void offset(double offset) = 0;
 
-    /**
-     * @brief Removes the offset from the dimension.
-     *
-     * This resets the offset to the default which is 0.
-     *
-     * @param t         None
-     */
+
     virtual void offset(const none_t t) = 0;
 
-    /**
-     * @brief Destructor
-     */
+
     virtual ~ISampledDimension() {}
 
 };
@@ -186,33 +106,15 @@ class NIXAPI ISetDimension : virtual public IDimension {
 
 public:
 
-    /**
-     * @brief Get the labels of the range dimension.
-     *
-     * The labels serve as names for each index of the data at the respective
-     * dimension.
-     *
-     * @return The labels of the dimension as a vector of strings.
-     */
     virtual std::vector<std::string> labels() const = 0;
 
-    /**
-     * @brief Set the labels for the dimension.
-     *
-     * @param labels    A vector containing all new labels.
-     */
+
     virtual void labels(const std::vector<std::string> &labels) = 0;
 
-    /**
-     * @brief Remove the labels from the dimension.
-     *
-     * @param t         None
-     */
+
     virtual void labels(const none_t t) = 0;
 
-    /**
-     * @brief Destructor
-     */
+
     virtual ~ISetDimension() {}
 
 };
@@ -227,79 +129,30 @@ class NIXAPI IRangeDimension : virtual public IDimension {
 
 public:
 
-    /**
-     * @brief Getter for the label of the dimension.
-     *
-     * The label of a RangeDimension corresponds to the axis label
-     * in a plot of the respective dimension.
-     *
-     * @return The label of the dimension.
-     */
     virtual boost::optional<std::string> label() const = 0;
 
-    /**
-     * @brief Sets the label of the dimension.
-     *
-     * @param label     The label of the dimension.
-     */
+
     virtual void label(const std::string &label) = 0;
 
-    /**
-     * @brief Remove the label of the dimension.
-     *
-     * @param t         None
-     */
+
     virtual void label(const none_t t) = 0;
 
-    /**
-     * @brief Gets the unit of a dimension.
-     *
-     * The unit describes which SI unit applies to this dimension
-     * and to its sampling interval.
-     *
-     * @return The unit of the dimension.
-     */
+
     virtual boost::optional<std::string> unit() const = 0;
 
-    /**
-     * @brief Sets the unit of a dimension.
-     *
-     * @param unit      The unit to set.
-     */
+
     virtual void unit(const std::string &unit) = 0;
 
-    /**
-     * @brief Removes the unit from a dimension.
-     *
-     * @param t         None
-     */
+
     virtual void unit(const none_t t) = 0;
 
-    /**
-     * @brief Get the ticks of the dimension.
-     *
-     * The ticks map the index of the data at the respective dimension to other
-     * values. This can be used to store data that is sampled at irregular
-     * intervals.
-     *
-     * Ticks must be ordered in ascending order.
-     *
-     * @return A vector with all ticks for the dimension.
-     */
+
     virtual std::vector<double> ticks() const = 0;
 
-    /**
-     * @brief Set the ticks vector for the dimension.
-     *
-     * Ticks must be ordered in ascending order.
-     *
-     * @param ticks     The new ticks for the dimension provided as a vector.
-     */
+
     virtual void ticks(const std::vector<double> &ticks) = 0;
 
-    /**
-     * @brief Destructor
-     */
+
     virtual ~IRangeDimension() {}
 
 };
