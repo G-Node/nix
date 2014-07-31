5054c86c55802387ffe0d6d3c096b233fce1e8d0
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-07-31 13:40:59 +0200

    Source: move comments

--- include/nix/base/ISource.hpp
+++ include/nix/base/ISource.hpp
@@ -32,66 +32,24 @@ class NIXAPI ISource : virtual public IEntityWithMetadata {
 
 public:
 
-    /**
-     * @brief Checks if this source has a specific source as direct descendant.
-     *
-     * @param id        The id of the source.
-     *
-     * @return True if a source with the given id is a direct descendant, false
-     *         otherwise.
-     */
     virtual bool hasSource(const std::string &id) const = 0;
 
-    /**
-     * @brief Retrieves a specific child source that is a direct descendant.
-     *
-     * @param id        The id of the source.
-     *
-     * @return The source with the given id. If it doesn't exist an exception
-     *         will be thrown.
-     */
+
     virtual Source getSource(const std::string &id) const = 0;
 
-    /**
-     * @brief Retrieves a specific source by index.
-     *
-     * @param index     The index of the source.
-     *
-     * @return The source at the specified index.
-     */
+
     virtual Source getSource(size_t index) const = 0;
 
-    /**
-     * @brief Returns the number of sources that are direct descendants of this source.
-     *
-     * @return The number of direct child sources.
-     */
+
     virtual size_t sourceCount() const = 0;
 
-    /**
-     * @brief Create a new root source.
-     *
-     * @param name      The name of the source to create.
-     * @param type      The type of the source.
-     *
-     * @return The created source object.
-     */
+
     virtual Source createSource(const std::string &name, const std::string &type) = 0;
 
-    /**
-     * @brief Delete a root source and all its child sources from
-     *        the source.
-     *
-     * @param id        The id of the source to remove.
-     *
-     * @return True if the source was deleted, false otherwise.
-     */
+
     virtual bool deleteSource(const std::string &id) = 0;
 
 
-    /**
-     * @brief Destructor
-     */
     virtual ~ISource() {}
 
 };
