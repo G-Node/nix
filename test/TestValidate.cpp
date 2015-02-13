c63a10ee01020f42889ecabf6d25d86819f91801
Jan Grewe <jan.grewe@g-node.org>
2015-02-13 15:45:44 +0100

    [validate] have setValid produce a valid file

--- test/TestValidate.cpp
+++ test/TestValidate.cpp
@@ -80,7 +80,7 @@ void TestValidate::setValid() {
         }
     }
     array4.setData(sin_array);
-    for (size_t i = 1; i <= array4.dimensionCount(); ++i) {
+    for (size_t i = array4.dimensionCount(); i > 0 ; --i) {
         array4.deleteDimension(i);
     }
     array4.appendSampledDimension(1.);
@@ -96,6 +96,13 @@ void TestValidate::setValid() {
                 A[i][j][k] = values++;
     array1.setData(A);
     array2.setData(A);
+    for (size_t i = array2.dimensionCount(); i > 0; --i) {
+        array2.deleteDimension(i);
+    }
+    dim_range1 = array2.appendRangeDimension({1, 2, 3});
+    dim_range2 = array2.appendRangeDimension({1, 2, 3, 4});
+    dim_range3 = array2.appendRangeDimension({1, 2});
+
     array3.setData(A);
 
     // fill extent & position
@@ -122,12 +129,12 @@ void TestValidate::setValid() {
     extents.setData(C);
     
     // ensure correct dimension descriptors for positions
-    for (size_t i = 1; i <= positions.dimensionCount(); ++i) {
+    for (size_t i = positions.dimensionCount(); i > 0; --i) {
         positions.deleteDimension(i);
     }
     positions.appendSetDimension();
     positions.appendSetDimension();
-    for (size_t i = 1; i <= extents.dimensionCount(); ++i) {
+    for (size_t i = extents.dimensionCount(); i > 0; --i) {
         extents.deleteDimension(i);
     }
     extents.appendSetDimension();
