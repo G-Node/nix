08ecd2eb90f5b9725289febbdc5bc3572f3fbd48
Jan Grewe <jan.grewe@g-node.org>
2015-02-13 10:53:30 +0100

    [validate] tag, if units then refs must have units, and they have to match

--- src/valid/validate.cpp
+++ src/valid/validate.cpp
@@ -129,9 +129,9 @@ Result validate(const Tag &tag) {
         }),
         // check units for validity
         could(tag, &Tag::units, notEmpty(), {
-            must(tag, &Tag::units, isValidUnit(), "Unit is invalid: not an atomic SI. Note: So far composite units are not supported!") }),
-        must(tag, &Tag::references, tagRefsHaveUnits(tag.units()), "Some of the referenced DataArrays' dimensions don't have units where the tag has. Make sure that all references have the same number of dimensions as the tag has units and that each dimension has a unit set."),
-        must(tag, &Tag::references, tagUnitsMatchRefsUnits(tag.units()), "Some of the referenced DataArrays' dimensions have units that are not convertible to the units set in tag. Note: So far composite SI units are not supported!"),
+            must(tag, &Tag::units, isValidUnit(), "Unit is invalid: not an atomic SI. Note: So far composite units are not supported!"),
+            must(tag, &Tag::references, tagRefsHaveUnits(tag.units()), "Some of the referenced DataArrays' dimensions don't have units where the tag has. Make sure that all references have the same number of dimensions as the tag has units and that each dimension has a unit set."),
+                must(tag, &Tag::references, tagUnitsMatchRefsUnits(tag.units()), "Some of the referenced DataArrays' dimensions have units that are not convertible to the units set in tag. Note: So far composite SI units are not supported!")}),
         // check positions & extents
         could(tag, &Tag::extent, notEmpty(), {
             must(tag, &Tag::position, extentsMatchPositions(tag.extent()), "Number of entries in position and extent do not match!"),
