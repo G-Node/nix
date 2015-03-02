7386fe6b4dfa29453cb5aaf10b331b4bc4e86d92
asobolev <sobolev.andrey@gmail.com>
2015-03-02 13:52:54 +0100

    changed data type struct to just map

--- src/DataType.cpp
+++ src/DataType.cpp
@@ -46,32 +46,28 @@ std::string data_type_to_string(DataType dtype) {
 
 DataType string_to_data_type(std::string dtype) {
 
-    struct data_type_map : public std::map<std::string, DataType> {
-        data_type_map() {
-            (*this)["bool"] = DataType::Bool;
-            (*this)["char"] = DataType::Char;
-            (*this)["float"] = DataType::Float;
-            (*this)["single"] = DataType::Float;
-            (*this)["double"] = DataType::Double;
-            (*this)["int8"] = DataType::Int8;
-            (*this)["int16"] = DataType::Int16;
-            (*this)["int32"] = DataType::Int32;
-            (*this)["int64"] = DataType::Int64;
-            (*this)["uint8"] = DataType::UInt8;
-            (*this)["uint16"] = DataType::UInt16;
-            (*this)["uint32"] = DataType::UInt32;
-            (*this)["uint64"] = DataType::UInt64;
-            (*this)["string"] = DataType::String;
-            (*this)["nothing"] = DataType::Nothing;
-        }
+    static std::map<std::string, DataType> type_map = {
+        {"bool", DataType::Bool},
+        {"char", DataType::Char},
+        {"float", DataType::Float},
+        {"single", DataType::Float},
+        {"double", DataType::Double},
+        {"int8", DataType::Int8},
+        {"int16", DataType::Int16},
+        {"int32", DataType::Int32},
+        {"int64", DataType::Int64},
+        {"uint8", DataType::UInt8},
+        {"uint16", DataType::UInt16},
+        {"uint32", DataType::UInt32},
+        {"uint64", DataType::UInt64},
+        {"string", DataType::String},
+        {"nothing", DataType::Nothing}
     };
-
-    static data_type_map dt;
 
     std::string dtype_l = dtype;
     std::transform(dtype_l.begin(), dtype_l.end(), dtype_l.begin(), ::tolower);
 
-    return dt[dtype];
+    return type_map[dtype];
 }
 
 std::ostream &operator<<(std::ostream &out, const DataType dtype) {
