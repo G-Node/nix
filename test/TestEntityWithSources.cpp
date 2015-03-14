197c6b559a8f9fa7e50dc42a1a385e45c5d79ecd
Jan Grewe <jan.grewe@g-node.org>
2015-03-14 20:08:45 +0100

    [TestEntityWithSources] add test for vector setter of sources

--- test/TestEntityWithSources.cpp
+++ test/TestEntityWithSources.cpp
@@ -62,3 +62,26 @@ void TestEntityWithSources::testSourceAccess() {
 }
 
 
+void TestEntityWithSources::testSourceVectorSetter() {
+    vector<string> names = { "source_a", "source_b", "source_c", "source_d", "source_e" };
+    vector<Source> sources;
+    DataArray da = block.createDataArray("Test","test", nix::DataType::Double, nix::NDSize {0,0});
+    for (auto it = names.begin(); it != names.end(); it++) {
+        sources.push_back(block.createSource(*it, "channel"));
+    }
+    CPPUNIT_ASSERT(block.sourceCount() == sources.size());
+    CPPUNIT_ASSERT(da.sourceCount() == 0);
+    
+    da.addSource(sources[0]);
+    da.addSource(sources[1]);
+    CPPUNIT_ASSERT(da.sourceCount() == 2);
+    da.sources(sources);
+    CPPUNIT_ASSERT(da.sourceCount() == sources.size());
+    
+    sources.clear();
+    sources.push_back(block.createSource("source_f", "channel"));
+    sources.push_back(block.createSource("source_g", "channel"));
+    da.sources(sources);
+    CPPUNIT_ASSERT(da.sourceCount() == sources.size());
+    CPPUNIT_ASSERT(block.sourceCount() == (sources.size() + names.size())); 
+}
