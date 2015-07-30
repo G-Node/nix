dd7b4a0798e73bedddfe68752f69ed9ce5f18148
Jan Grewe <jan.grewe@g-node.org>
2015-07-30 10:48:47 +0200

    [file backend] add filesysAttributes class

---
+++ src/filesys/FilesystemAttribute.cpp
@@ -0,0 +1,37 @@
+//
+// Created by jan on 7/30/15.
+//
+
+#include <nix/filesys/FilesystemAttribute.hpp>
+
+using namespace nix::filesys;
+using namespace boost::filesystem;
+using namespace YAML;
+
+FilesystemAttribute::FilesystemAttribute() { }
+
+
+FilesystemAttribute::FilesystemAttribute(const std::string &file_path) {
+    path temp(file_path.c_str());
+    if (exists(temp)) {
+        this->loc = temp;
+        this->open_or_create();
+   }
+}
+
+
+void FilesystemAttribute::open_or_create() {
+    path attr(".attributes");
+    path temp = this->location() /  attr;
+    if (!exists(temp)) {
+        std::ofstream ofs;
+        ofs.open(this->location().string() + "/.attributes", std::ofstream::out | std::ofstream::app);
+        ofs.close();
+    }
+    this->node = Load(this->location().string() + "/.attributes");
+}
+
+
+path FilesystemAttribute::location() const {
+    return this->loc;
+}
