d9736f5eca46fdb3c6eb98bb12dede906fc0f3c0
Jan Grewe <jan.grewe@g-node.org>
2015-08-31 18:47:44 +0200

    [file_backend/TestBaseTag] add filesys tests

--- test/TestBaseTag.cpp
+++ test/TestBaseTag.cpp
@@ -1,4 +1,4 @@
-// Copyright © 2014 German Neuroinformatics Node (G-Node)
+// Copyright © 2014 - 2015 German Neuroinformatics Node (G-Node)
 //
 // All rights reserved.
 //
@@ -27,7 +27,15 @@ void TestBaseTag::setUp() {
     refs.clear();
     for (const auto & name : array_names) {
         refs.push_back(block.createDataArray(name, "reference",
-                DataType::Double, nix::NDSize({ 0 })));
+                                             DataType::Double, nix::NDSize({ 0 })));
+    }
+
+    file_fs = File::open("test_basetag", FileMode::Overwrite, Implementation::FileSys);
+    block_fs = file_fs.createBlock("block", "dataset");
+    refs_fs.clear();
+    for (const auto & name : array_names) {
+        refs_fs.push_back(block_fs.createDataArray(name, "reference",
+                                                   DataType::Double, nix::NDSize({ 0 })));
     }
 }
 
@@ -40,14 +48,17 @@ void TestBaseTag::tearDown() {
 //TODO Constraints on References are not tested yet.
 
 void TestBaseTag::testReferences() {
-    DataArray da_1 = block.createDataArray("TestReference 1","Reference",
-            DataType::Double, nix::NDSize({ 0 }));
-    DataArray da_2 = block.createDataArray("TestReference 2","Reference",
-            DataType::Double, nix::NDSize({ 0 }));
+    test_references(block, refs);
+    test_references(block_fs, refs_fs);
+}
 
-    Tag st = block.createTag("TestTag1", "Tag", {0.0, 2.0, 3.4});
-    st.references(refs);
-
+void TestBaseTag::test_references(Block &b, vector<DataArray> &r) {
+    DataArray da_1 = b.createDataArray("TestReference 1","Reference",
+                                       DataType::Double, nix::NDSize({ 0 }));
+    DataArray da_2 = b.createDataArray("TestReference 2","Reference",
+                                       DataType::Double, nix::NDSize({ 0 }));
+    Tag st = b.createTag("TestTag1", "Tag", {0.0, 2.0, 3.4});
+    st.references(r);
     CPPUNIT_ASSERT_THROW(st.getReference(42), nix::OutOfBounds);
 
     std::stringstream counterrmsg;
@@ -66,10 +77,8 @@ void TestBaseTag::testReferences() {
     DataArray ref2 = st.getReference(da_1.name());
     retrieveerrmsg << "TestTag::testReference: Retrieval by name did not work!";
     CPPUNIT_ASSERT_MESSAGE(retrieveerrmsg.str(), ref2.id() == da_1.id());
-
     std::vector<DataArray> arrays = st.references();
     CPPUNIT_ASSERT_MESSAGE(retrieveerrmsg.str(), arrays.size() == 7);
-
     std::stringstream hasReferrmsg;
     hasReferrmsg << "TestTag::testReference: hadReference did not work!";
     CPPUNIT_ASSERT_MESSAGE(hasReferrmsg.str(), st.hasReference(da_1.id()));
@@ -84,42 +93,58 @@ void TestBaseTag::testReferences() {
 
     // delete data arrays
     std::vector<std::string> ids;
-    block.deleteDataArray(da_1.id());
-    block.deleteDataArray(da_2.id());
-    for (auto it = refs.begin(); it != refs.end(); it++) {
+    b.deleteDataArray(da_1.id());
+    b.deleteDataArray(da_2.id());
+    for (auto it = r.begin(); it != r.end(); it++) {
         ids.push_back((*it).id());
-        block.deleteDataArray((*it).id());
+        b.deleteDataArray((*it).id());
     }
     // check if references are gone too!
     CPPUNIT_ASSERT(st.referenceCount() == 0);
     for (auto ref_id : ids) {
         CPPUNIT_ASSERT(!st.hasReference(ref_id));
     }
-    block.deleteTag(st.id());
+    b.deleteTag(st.id());
 }
 
-
 void TestBaseTag::testFeatures() {
-    Tag st = block.createTag("TestTag", "tag", {10.0});
-    DataArray da1 = block.createDataArray("featureArray1", "test", nix::DataType::Double, {1});
-    DataArray da2 = block.createDataArray("featureArray2", "test", nix::DataType::Double, {1});
-    DataArray da3 = block.createDataArray("featureArray3", "test", nix::DataType::Double, {1});
+    test_features(block);
+    test_features(block_fs);
+}
+
+void TestBaseTag::test_features(Block &b) {
+    Tag st = b.createTag("TestTag", "tag", {10.0});
+    DataArray da1 = b.createDataArray("featureArray1", "test", nix::DataType::Double, {1});
+    DataArray da2 = b.createDataArray("featureArray2", "test", nix::DataType::Double, {1});
+    DataArray da3 = b.createDataArray("featureArray3", "test", nix::DataType::Double, {1});
 
     int failures = 0;
     for (int i = 0; i < 20; i++) {
+        vector<string> ids;
         Feature f1 = st.createFeature(da1, nix::LinkType::Untagged);
+        ids.push_back(f1.id());
         Feature f2 = st.createFeature(da2, nix::LinkType::Tagged);
+        ids.push_back(f2.id());
         Feature f3 = st.createFeature(da3, nix::LinkType::Untagged);
-        if (st.getFeature(0).id() != f1.id() || st.getFeature(1).id() != f2.id() || st.getFeature(2).id() != f3.id()) {
-            failures ++;
+        ids.push_back(f3.id());
+        CPPUNIT_ASSERT(st.featureCount() == 3);
+        for (ndsize_t j = 0; j < st.featureCount(); j ++) {
+            if (std::find(ids.begin(), ids.end(), st.getFeature(j).id()) == ids.end())
+                failures ++;
         }
+        CPPUNIT_ASSERT(st.getFeature(f1.id()).id() == f1.id());
+        CPPUNIT_ASSERT(st.getFeature(f2.id()).id() == f2.id());
+        CPPUNIT_ASSERT(st.getFeature(f3.id()).id() == f3.id());
+        //CPPUNIT_ASSERT_THROW(st.getFeature(""), EmptyString);
         st.deleteFeature(f1.id());
         st.deleteFeature(f2.id());
         st.deleteFeature(f3.id());
+        CPPUNIT_ASSERT(st.featureCount() == 0);
     }
-    block.deleteTag(st.id());
-    block.deleteDataArray(da1.id());
-    block.deleteDataArray(da2.id());
-    block.deleteDataArray(da3.id());
+    CPPUNIT_ASSERT_THROW(st.getFeature(100), OutOfBounds);
+    b.deleteTag(st.id());
+    b.deleteDataArray(da1.id());
+    b.deleteDataArray(da2.id());
+    b.deleteDataArray(da3.id());
     CPPUNIT_ASSERT(failures == 0);
 }
