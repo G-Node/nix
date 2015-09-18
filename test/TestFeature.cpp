39e8c115d431290c08a46696db173778ab30447b
Jan Grewe <jan.grewe@g-node.org>
2015-09-18 18:10:51 +0200

    [TestFeature] switch to uninitializedEntity exception

--- test/TestFeature.cpp
+++ test/TestFeature.cpp
@@ -72,7 +72,7 @@ void TestFeature::testData() {
     DataArray a;
     Feature f;
     CPPUNIT_ASSERT_THROW(tag.createFeature(a, nix::LinkType::Tagged), std::runtime_error);
-    CPPUNIT_ASSERT_THROW(f.data(a), std::runtime_error);
+    CPPUNIT_ASSERT_THROW(f.data(a), UninitializedEntity);
     Feature rp = tag.createFeature(data_array, nix::LinkType::Tagged);
     DataArray da_2 = block.createDataArray("array2", "Test",
                                            DataType::Double, nix::NDSize({ 0 }));
