5b874b42e8c81868421416aa4879cc2df0748157
Jan Grewe <jan.grewe@g-node.org>
2015-09-22 22:52:37 +0200

    [Section] move getSection to frontend

--- src/Section.cpp
+++ src/Section.cpp
@@ -325,3 +325,10 @@ Property Section::createProperty(const std::string &name, const Value &value) {
     }
     return backend()->createProperty(name, value);
 }
+
+Section Section::getSection(ndsize_t index) const {
+    if (index >= backend()->sectionCount()) {
+        throw OutOfBounds("Section::getSection: index is out of bounds!");
+    }
+    return backend()->getSection(index);
+}
