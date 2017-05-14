0d864f946fa3e7894fad877a7f0ac459ece13329
Christian Kellner <christian@kellner.me>
2017-05-14 17:55:31 +0200

    Editors: add editorconfig, ns indent for emacs
    Help editors doing the right thing for various files using the
    editorconfig standard: http://editorconfig.org

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
-((c++-mode .
-	   (
-	    (indent-tabs-mode . nil)
-	    (tab-width . 4)
-	    (c-basic-offset . 4)
-	    (fill-column . 80))))
+
+((c++-mode . ((indent-tabs-mode . nil)
+              (tab-width . 4)
+              (c-basic-offset . 4)
+              (fill-column . 80)
+              (c-offsets-alist . ((innamespace . [0]))))))
