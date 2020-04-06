3b8a1eaa4adaf851e6ac9f30a612b07c69b7a535
Jan Grewe <jngrewe@googlemail.com>
2020-04-06 19:42:55 +0200

    Update TestDataType.cpp

--- test/TestDataType.cpp
+++ test/TestDataType.cpp
@@ -34,7 +34,7 @@ void TestDataType::setUp() {
                 "uint8", "uint16", "uint32", "uint64",
                 "string", "nothing",  "opaque"};
     fixed_sizes = decltype(fixed_sizes) {{nix::DataType::Int8, 1},
-                                         {nix::DataType::Int8, 1},
+                                         {nix::DataType::UInt8, 1},
                                          {nix::DataType::Int16, 2},
                                          {nix::DataType::UInt16, 2},
                                          {nix::DataType::Int32, 4},
