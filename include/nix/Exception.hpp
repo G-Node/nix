d3b8ae57f7b5668454fdb117e5a1d3e4b9abf6ab
Jan Grewe <jan.grewe@g-node.org>
2020-04-17 17:15:51 +0200

    [Exceptions] converts_to_double throws exception, if failing

--- include/nix/Exception.hpp
+++ include/nix/Exception.hpp
@@ -151,10 +151,13 @@ fits_in_size_t(T size, const std::string &msg_if_fail) {
 }
 
 template<typename T>
-inline typename std::enable_if<std::is_integral<T>::value, bool>::type
-converts_to_double(T num, double &dbl) {
-    dbl = static_cast<double>(num);
-    return static_cast<T>(dbl) == num;
+inline typename std::enable_if<std::is_integral<T>::value, double>::type
+converts_to_double(T num, const std::string &msg_if_fail) {
+    double dbl = static_cast<double>(num);
+    if (static_cast<T>(dbl) != num) {
+        throw OutOfBounds(msg_if_fail);
+    }
+    return dbl;
 }
 
 } // nix::check::
